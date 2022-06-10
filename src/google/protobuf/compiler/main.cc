3f5d91f2e169d890164d3401b8f4a9453fff5538
Ricky Stewart <ricky@cockroachlabs.com>
2022-06-10 11:26:49 -0500

    protoc: delete unused proto generators
    be propagated to the `bazel` build. I do that and outright delete all
    the unused generators so they are not accidentally compiled for any
    reason.

--- src/google/protobuf/compiler/main.cc
+++ src/google/protobuf/compiler/main.cc
@@ -30,16 +30,9 @@
 
 // Author: kenton@google.com (Kenton Varda)
 
-#include <google/protobuf/compiler/cpp/cpp_generator.h>
-#include <google/protobuf/compiler/java/java_generator.h>
 #include <google/protobuf/compiler/command_line_interface.h>
-#include <google/protobuf/compiler/python/python_generator.h>
 
-#include <google/protobuf/compiler/csharp/csharp_generator.h>
 #include <google/protobuf/compiler/js/js_generator.h>
-#include <google/protobuf/compiler/objectivec/objectivec_generator.h>
-#include <google/protobuf/compiler/php/php_generator.h>
-#include <google/protobuf/compiler/ruby/ruby_generator.h>
 
 #include <google/protobuf/port_def.inc>
 
@@ -51,47 +44,6 @@ int ProtobufMain(int argc, char* argv[]) {
 
   CommandLineInterface cli;
   cli.AllowPlugins("protoc-");
-
-  // Proto2 C++
-  cpp::CppGenerator cpp_generator;
-  cli.RegisterGenerator("--cpp_out", "--cpp_opt", &cpp_generator,
-                        "Generate C++ header and source.");
-
-#ifdef GOOGLE_PROTOBUF_RUNTIME_INCLUDE_BASE
-  cpp_generator.set_opensource_runtime(true);
-  cpp_generator.set_runtime_include_base(GOOGLE_PROTOBUF_RUNTIME_INCLUDE_BASE);
-#endif
-
-  // Proto2 Java
-  // java::JavaGenerator java_generator;
-  // cli.RegisterGenerator("--java_out", "--java_opt", &java_generator,
-  //                       "Generate Java source file.");
-
-
-  // Proto2 Python
-  python::Generator py_generator;
-  cli.RegisterGenerator("--python_out", &py_generator,
-                        "Generate Python source file.");
-
-  // PHP
-  php::Generator php_generator;
-  cli.RegisterGenerator("--php_out", &php_generator,
-                        "Generate PHP source file.");
-
-  // Ruby
-  ruby::Generator rb_generator;
-  cli.RegisterGenerator("--ruby_out", &rb_generator,
-                        "Generate Ruby source file.");
-
-  // CSharp
-  // csharp::Generator csharp_generator;
-  // cli.RegisterGenerator("--csharp_out", "--csharp_opt", &csharp_generator,
-  //                       "Generate C# source file.");
-
-  // Objective C
-  // objectivec::ObjectiveCGenerator objc_generator;
-  // cli.RegisterGenerator("--objc_out", "--objc_opt", &objc_generator,
-  //                       "Generate Objective C header and source.");
 
   // JavaScript
   js::Generator js_generator;
